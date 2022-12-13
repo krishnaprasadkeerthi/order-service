569d0d0d4f5bda5bc990b38155745dd025df84f5
KrishnaPrasad K S <krishnaprasad1287@gmail.com>
2022-12-13 08:59:24 +0530

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -15,7 +15,7 @@ public class OrderDao {
                 new Order(101, "Mobile", 1, 60000),
                 new Order(58, "Book", 4, 4000),
                 new Order(205, "Laptop", 1, 170000),
-                new Order(809, "headset", 1, 1999))
+                new Order(809, "headset", 1, 2999))
                 .collect(Collectors.toList());
     }
 }
